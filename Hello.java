ca74c071d98063dd8b9a2e889385a74a5d6be5e7
tresoles <48192916+tresoles@users.noreply.github.com>
2019-08-21 14:06:16 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/* Hello.java
+Introduction to the Course
+Trevon Lamb
+August 21, 2019
+CMSC 255 Section 002 */
+
+public class Hello {
+    public static void printHeading() {
+        System.out.println("Trevon Lamb");
+        System.out.println("Hello.java");
+        System.out.println("Introduction to the Course");
+        System.out.println("August 21,2019");
+        System.out.println("CMSC 255 Section 002");
+        System.out.println();
+    }
+    public static void main(String [] args) {
+       printHeading();
+       System.out.println("Hello"); 
+    }
+}
